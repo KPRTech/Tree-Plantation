fdcfcfd7977e6975dd3f3f6a675456721c24f66c
Sumyia <sumyia.mubassira@gmail.com>
2023-01-30 19:46:37 +0600

    image added

--- tree-plantation-f/src/components/Home.js
+++ tree-plantation-f/src/components/Home.js
@@ -32,28 +32,11 @@ const Home = () => {
                     <div class="flex flex-col text-center w-full mb-20">
                         <h1 class="sm:text-4xl text-2xl font-medium title-font mb-4 text-gray-500">TYPES OF TREES</h1>
                         <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Did you know that the world is home to over 60,000 different tree species? Trees provide innumerous benefits such as clean air, water, biodiversity, social impact, health and help to regulate climate. </p>
-
-
-
                         <p class="lg:w-2/3 mx-auto leading-rellaxed text-base"> Read on to find out the main types of trees, where each tree species is located, common tree planting regions and some fun tree facts! </p>
-
-
-
                     </div>
-
-
-
-
-
-
-
-
                     <div class="flex flex-wrap -m-4 text-center">
                         <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
-
                         </div>
-
-
                     </div>
                 </div>
             </section>
@@ -193,10 +176,7 @@ const Home = () => {
                                     <p class="leading-relaxed mb-3 text-center">Beechnut was used as food for the cattle. Many forest mammals and birds consume beechnuts as a regular part of their diet</p>
                                     <div class="flex items-center flex-wrap ">
 
-                                        <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                            <path d="M5 12h14"></path>
-                                            <path d="M12 5l7 7-7 7"></path>
-                                        </svg>
+
 
 
 
@@ -269,10 +249,7 @@ const Home = () => {
                                     <p class="leading-relaxed mb-3 text-blue-400 text-center ">North America</p>
                                     <div class="flex items-center flex-wrap ">
 
-                                        <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                            <path d="M5 12h14"></path>
-                                            <path d="M12 5l7 7-7 7"></path>
-                                        </svg>
+
 
 
 
@@ -305,7 +282,7 @@ const Home = () => {
                     <div class="flex flex-wrap -m-4">
                         <div class="p-4 md:w-1/4">
                             <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                                <img class="lg:h-40md:h-36w-100 object-cover object-center" src="/image/african_junifer.webp" alt="blog"></img>
+                                <img class="lg:h-40md:h-36w-100 object-cover object-center" src="/image/imageA.webp" alt="blog"></img>
                                 <div class="p-6">
                                     <h1 class="title-font text-lg font-medium text-gray-900 mb-3 text-center">AMERICAN ELM</h1>
                                     <p class="leading-relaxed mb-3 text-center">The wood is strong and coarse-grained; largely used for veneer, crates and wheel hubs. It has nearly disappeared because of the Dutch Elm disease.  </p>
@@ -320,10 +297,11 @@ const Home = () => {
                         </div>
                         <div class="p-4 md:w-1/4">
                             <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                                <img class="lg:h-40 md:h-36 w-100 object-cover object-center" src="/image/image4.webp" alt="blog"></img>
+                                <img class="lg:h-40 md:h-36 w-100 object-cover object-center" src="/image/imageB.webp" alt="blog"></img>
                                 <div class="p-6">
-                                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">AMERICAN ARBORVITAE</h1>
-                                    <p class="leading-relaxed mb-3">Meaning "tree of life", this medium-sized forest tree grows to a height of 40-60 ft. Was useful in early canoes and medicines and became the first North American tree to be introduced to Europe.</p>
+                                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3 text-center">AMERICAN HORNBEAM</h1>
+
+                                    <p class="leading-relaxed mb-3 text-center">The tree is slow-growing and is rarely found larger than 10 inches in diameter. It makes excellent fuelwood when seasoned.</p>
                                     <div class="flex items-center flex-wrap">
 
 
@@ -333,17 +311,14 @@ const Home = () => {
                         </div>
                         <div class="p-4 md:w-1/4">
                             <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                                <img class="lg:h-40 md:h-36 w-100 object-cover object-center" src="/image/imge5.jpg" alt="blog"></img>
+                                <img class="lg:h-40 md:h-36 w-100 object-cover object-center" src="/image/imageC.webp" alt="blog"></img>
                                 <div class="p-6">
 
-                                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">AMERICAN BEECH</h1>
-                                    <p class="leading-relaxed mb-3">One of the most well-known trees in the state, given its wide distribution across forests. It is excellent as fuelwood. The disease on the bark of beech trees is currently threatening the species across its entire range.</p>
+                                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3 text-center">AMERICAN LARCH</h1>
+                                    <p class="leading-relaxed mb-3 text-center">A forest tree of the swamps. The wood is very heavy, and strong, it is used for fence posts, telegraph poles, and railroad ties. </p>
                                     <div class="flex items-center flex-wrap ">
 
-                                        <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                            <path d="M5 12h14"></path>
-                                            <path d="M12 5l7 7-7 7"></path>
-                                        </svg>
+
 
 
 
@@ -353,11 +328,11 @@ const Home = () => {
                         </div>
                         <div class="p-4 md:w-1/4">
                             <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                                <img class="lg:h-40 md:h-36 w-100 object-cover object-center" src="/image/ima.webp" alt="blog"></img>
+                                <img class="lg:h-40 md:h-36 w-100 object-cover object-center" src="/image/imageD.webp" alt="blog"></img>
                                 <div class="p-6">
 
-                                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">AMERICAN CHESTNUT</h1>
-                                    <p class="leading-relaxed mb-3">The American chestnut tree was once a dominant forest species, but due to a blight disease, is now rare. Before the blight, trees grew close to 115 ft tall, now surviving trees are smaller than 32 ft.</p>
+                                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3 text-center">AMERICAN MOUNTAINASH</h1>
+                                    <p class="leading-relaxed mb-3 text-center">A relatively small tree, reaching only 40 ft in height. The American mountain-ash attains its largest pecimens on the northern shores of Lake Huron and Lake Superios</p>
                                     <div class="flex items-center flex-wrap">
 
 
@@ -370,106 +345,74 @@ const Home = () => {
 
             </section>
 
+            <section id="a" class="text-gray-600 body-font">
 
-
-
-
-            <section id="b" class="text-gray-600 body-font">
-                <span style={{ fontSize: "60px" }}>B</span>
                 <div class="container px-5 py-24 mx-auto">
                     <div class="flex flex-wrap -m-4">
-                        <div class="p-4 md:w-1/3">
+                        <div class="p-4 md:w-1/4">
                             <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                                <img class=""></img>
+
+                                <div class="p-6">
+                                    <h1 class="title-font text-lg font-medium text-green-500 mb-3 text-center ">FUN FACT</h1>
+                                    <p class="lg:w-2/2 mx-auto leading-relaxed text-base text-center"> It is the only juniper to occur south of the equator. </p>
+                                    <div class="flex items-center flex-wrap ">
+
+
+
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="p-4 md:w-1/4">
+                            <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+
+                                <div class="p-6">
+                                    <h1 class="title-font text-lg font-medium text-green-500 mb-3 text-center">FUN FACT</h1>
+                                    <p class="leading-relaxed mb-3 text-center ">The specific name, "Occidentalis", means "west," the direction from Sweden where this tree was discovered.</p>
+                                    <div class="flex items-center flex-wrap">
+
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="p-4 md:w-1/4">
+                            <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+
                                 <div class="p-6">
 
-                                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">hggh</h1>
-                                    <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                                    <h1 class="title-font text-lg font-medium text-green-500 mb-3 text-center">FUN FACT</h1>
+                                    <p class="leading-relaxed mb-3 text-center">Beechnut was used as food for the cattle. Many forest mammals and birds consume beechnuts as a regular part of their diet</p>
                                     <div class="flex items-center flex-wrap ">
-                                        <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
-                                            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                                <path d="M5 12h14"></path>
-                                                <path d="M12 5l7 7-7 7"></path>
-                                            </svg>
-                                        </a>
-                                        <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                                            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                                                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                                                <circle cx="12" cy="12" r="3"></circle>
-                                            </svg>1.2K
-                                        </span>
-                                        <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                                            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                                                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                                            </svg>6
-                                        </span>
+
+
+
+
+
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="p-4 md:w-1/3">
+                        <div class="p-4 md:w-1/4">
                             <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                                <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="download.jpg" alt="blog"></img>
+
                                 <div class="p-6">
-                                    <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-                                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The 400 Blows</h1>
-                                    <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+
+                                    <h1 class="title-font text-lg font-medium text-green-500 mb-3 text-center">FUN FACT</h1>
+                                    <p class=" leading-relaxed mb-3 text-center">Chestnuts can be consumed raw, baked, boiled or roasted. It also can be dried and milled into flour.
+                                    </p>
                                     <div class="flex items-center flex-wrap">
-                                        <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
-                                            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                                <path d="M5 12h14"></path>
-                                                <path d="M12 5l7 7-7 7"></path>
-                                            </svg>
-                                        </a>
-                                        <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                                            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                                                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                                                <circle cx="12" cy="12" r="3"></circle>
-                                            </svg>1.2K
-                                        </span>
-                                        <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                                            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                                                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                                            </svg>6
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="p-4 md:w-1/3">
-                            <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                                <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/722x402" alt="blog"></img>
-                                <div class="p-6">
-                                    <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-                                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
-                                    <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-                                    <div class="flex items-center flex-wrap ">
-                                        <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
-                                            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                                <path d="M5 12h14"></path>
-                                                <path d="M12 5l7 7-7 7"></path>
-                                            </svg>
-                                        </a>
-                                        <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                                            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                                                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                                                <circle cx="12" cy="12" r="3"></circle>
-                                            </svg>1.2K
-                                        </span>
-                                        <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                                            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                                                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                                            </svg>6
-                                        </span>
+
+
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-
                 </div>
-            </section>
 
+            </section>
 
         </div>
 
