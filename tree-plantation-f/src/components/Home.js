d4fb5849fa405df8792620245c02f480d89f9cf1
Sumyia <sumyia.mubassira@gmail.com>
2023-01-30 09:13:22 +0600

    added image

--- tree-plantation-f/src/components/Home.js
+++ tree-plantation-f/src/components/Home.js
@@ -3,32 +3,219 @@ import css from "./Home.module.css";
 
 const Home = () => {
     return (
-        <div className={css.bg}>
+        <div>
+            <div className={css.bg}>
 
-            <h1 style={{
-                textAlign: "center", color: "gray"
-                , fontSize: "60px"
-            }}>
-                TYPES OF TREE</h1>
+                <h1 style={{
+                    textAlign: "center", color: "gray"
+                    , fontSize: "60px"
+                }}>
+                    TYPES OF TREE</h1>
 
-            <p style={{
-                textAlign: "center", color: "gray"
-                , fontSize: "25px"
-            }}>LEARN ABOUT THE  DiFFERENT
-                TYPES OF TREE SPECIES</p>
-            <div style={{
-                textAlign: "center"
-            }}>
-                <button style={{
-                    textAlign: "center", color: "black",
-                    width: "200px",
-                    padding: "0px",
-                    boxSizing: "border-box",
-                    verticalAlign: "middle",
-                    fontSize: "25px",
-                }}>PLANT A TREE
-                </button>
+                <p style={{
+                    textAlign: "center", color: "gray"
+                    , fontSize: "25px"
+                }}>LEARN ABOUT THE  DIFFERENT
+                    TYPES OF TREE SPECIES</p>
+                <div style={{
+                    textAlign: "center",borderSize:"border-box"
+                }}>
+                    <button>
+                    
+                    PLANT A TREE
+                    </button>
+                    
+                </div>
             </div>
+            <div style={{ textAlign: "center" }}>
+                <a href='#a'><span style={{ fontSize: "25px",color:"green" 
+}}>A</span></a>
+                <a href='#a'><span style={{ fontSize: "25px",color:"green" }}>A</span></a>
+                <a href='#b'><span style={{ fontSize: "25px",color:"green"  }}>B</span></a>
+                <a href='#c'><span style={{ fontSize: "25px" ,color:"green" }}>C</span></a>
+                <a href='#d'><span style={{ fontSize: "25px",color:"green" }}>D</span></a>
+                <a href='#e'><span style={{ fontSize: "25px" ,color:"green"}}>E</span></a>
+                <a href='#f'><span style={{ fontSize: "25px" ,color:"green"}}>F</span></a>
+                <a href='#g'><span style={{ fontSize: "25px" ,color:"green"}}>G</span></a>
+                <a href='#h'><span style={{ fontSize: "25px",color:"green" }}>H</span></a>
+                <a href='#i'><span style={{ fontSize: "25px",color:"green" }}>I</span></a>
+                <a href='#j'><span style={{ fontSize: "25px" ,color:"green"}}>J</span></a>
+                <a href='#l'><span style={{ fontSize: "25px",color:"green" }}>L</span></a>
+                <a href='#m'><span style={{ fontSize: "25px" ,color:"green"}}>M</span></a>
+                <a href='#n'><span style={{ fontSize: "25px" ,color:"green"}}>N</span></a>
+                <a href='#o'><span style={{ fontSize: "25px" ,color:"green"}}>O</span></a>
+                <a href='#p'><span style={{ fontSize: "25px" ,color:"green"}}>P</span></a>
+                <a href='#q'><span style={{ fontSize: "25px" ,color:"green"}}>Q</span></a>
+                <a href='#r'><span style={{ fontSize: "25px" ,color:"green"}}>R</span></a>
+                <a href='#s'><span style={{ fontSize: "25px" ,color:"green"}}>S</span></a>
+                <a href='#t'><span style={{ fontSize: "25px" ,color:"green"}}>T</span></a>
+                <a href='#u'><span style={{ fontSize: "25px" ,color:"green"}}>U</span></a>
+                <a href='#w'><span style={{ fontSize: "25px" ,color:"green"}}>W</span></a>
+                
+            </div>
+            <section id="a" class="text-gray-600 body-font">
+                <span style={{ fontSize: "60px" }}>A</span>
+                <div class="container px-5 py-24 mx-auto">
+                    <div class="flex flex-wrap -m-4">
+                        <div class="p-4 md:w-1/4">
+                            <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                                <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="/image/african_junifer.webp" alt="blog"></img>
+                                <div class="p-6">
+                                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">AFRICAN JUNIPER</h1>
+                                    <p class="leading-relaxed mb-3">Native to mountainous areas in Africa and the Arabian Peninsula, The African Juniper Tree is an important timber tree used for building houses, for poles, for furniture; bark used for beehive</p>
+                                    <div class="flex items-center flex-wrap ">
+
+
+
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="p-4 md:w-1/4">
+                            <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                                <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/721x401" alt="blog"></img>
+                                <div class="p-6">
+                                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">AMERICAN ARBORVITAE</h1>
+                                    <p class="leading-relaxed mb-3">Meaning "tree of life", this medium-sized forest tree grows to a height of 40-60 ft. Was useful in early canoes and medicines and became the first North American tree to be introduced to Europe.</p>
+                                    <div class="flex items-center flex-wrap">
+                                      
+                                       
+                                                                          </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="p-4 md:w-1/4">
+                            <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                                <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/722x402" alt="blog"></img>
+                                <div class="p-6">
+
+                                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">AMERICAN BEECH</h1>
+                                    <p class="leading-relaxed mb-3">One of the most well-known trees in the state, given its wide distribution across forests. It is excellent as fuelwood. The disease on the bark of beech trees is currently threatening the species across its entire range.</p>
+                                    <div class="flex items-center flex-wrap ">
+                                        
+                                            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path d="M5 12h14"></path>
+                                                <path d="M12 5l7 7-7 7"></path>
+                                            </svg>
+                                        
+                                     
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="p-4 md:w-1/4">
+                            <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                                <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/721x401" alt="blog"></img>
+                                <div class="p-6">
+
+                                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">AMERICAN CHESTNUT</h1>
+                                    <p class="leading-relaxed mb-3">The American chestnut tree was once a dominant forest species, but due to a blight disease, is now rare. Before the blight, trees grew close to 115 ft tall, now surviving trees are smaller than 32 ft.</p>
+                                    <div class="flex items-center flex-wrap">
+                                       
+                                       
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="b" class="text-gray-600 body-font">
+                <span style={{ fontSize: "60px" }}>B</span>
+                <div class="container px-5 py-24 mx-auto">
+                    <div class="flex flex-wrap -m-4">
+                        <div class="p-4 md:w-1/3">
+                            <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                                <img class=""></img>
+                                <div class="p-6">
+
+                                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">hggh</h1>
+                                    <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                                    <div class="flex items-center flex-wrap ">
+                                        <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                                            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path d="M5 12h14"></path>
+                                                <path d="M12 5l7 7-7 7"></path>
+                                            </svg>
+                                        </a>
+                                        <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                                            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                                                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                                                <circle cx="12" cy="12" r="3"></circle>
+                                            </svg>1.2K
+                                        </span>
+                                        <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                                            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                                                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                                            </svg>6
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="p-4 md:w-1/3">
+                            <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                                <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="download.jpg" alt="blog"></img>
+                                <div class="p-6">
+                                    <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
+                                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The 400 Blows</h1>
+                                    <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                                    <div class="flex items-center flex-wrap">
+                                        <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                                            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path d="M5 12h14"></path>
+                                                <path d="M12 5l7 7-7 7"></path>
+                                            </svg>
+                                        </a>
+                                        <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                                            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                                                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                                                <circle cx="12" cy="12" r="3"></circle>
+                                            </svg>1.2K
+                                        </span>
+                                        <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                                            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                                                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                                            </svg>6
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="p-4 md:w-1/3">
+                            <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                                <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/722x402" alt="blog"></img>
+                                <div class="p-6">
+                                    <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
+                                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
+                                    <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                                    <div class="flex items-center flex-wrap ">
+                                        <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                                            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path d="M5 12h14"></path>
+                                                <path d="M12 5l7 7-7 7"></path>
+                                            </svg>
+                                        </a>
+                                        <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                                            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                                                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                                                <circle cx="12" cy="12" r="3"></circle>
+                                            </svg>1.2K
+                                        </span>
+                                        <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                                            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                                                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                                            </svg>6
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
