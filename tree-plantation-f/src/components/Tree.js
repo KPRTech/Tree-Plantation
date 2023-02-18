422aa46b808c4f74d9924b0d511558ebc9f6d139
Sumyia <sumyia.mubassira@gmail.com>
2023-02-18 23:43:29 +0600

    added tree.js

--- tree-plantation-f/src/components/Tree.js
+++ tree-plantation-f/src/components/Tree.js
@@ -8,29 +8,71 @@ const Tree = () => {
 
                 <h1 style={{
                     textAlign: "center",
-                    color: "#fff",
-                    fontFamily: "Raleway",
-                    fontWeight: "800",
-                    fontSize: "80px",
+                    color: "#ffff",
+                    fontFamily: "Ralway",
+                    fontWeight: "1000",
+                    fontSize: "45px",
                     marginBottom: "0",
                     lineHeight: "1em",
                     border: "0"
 
                 }}>
-                    TYPES OF TREE</h1>
+                    WE ARE</h1>
 
+                <h1 style={{
+                    textAlign: "center",
+                    color: "#ffff",
+                    fontFamily: "Raleway",
+                    fontWeight: "900",
+                    fontSize: "60px",
+                    marginBottom: "0",
+                    lineHeight: "1em",
+                    border: "0"
+
+                }}>ONE TREE PLANTED
+                </h1>
                 <p style={{
                     textAlign: "center", color: "#fff",
                     fontSize: "25px",
                     fontFamily: "Raleway",
-                    fontWeight: "50",
-
+                    fontWeight: "60",
+                    fontWeight: "600",
                     marginBottom: "0",
-                    lineHeight: "1em",
-                }}>LEARN ABOUT THE  DIFFERENT
-                    TYPES OF TREE SPECIES</p>
+                    lineHeight: "2em",
+                }}>A NON-PROFIT ORGANIZATION FOCUSED ON GLOBAL REFORESTATION</p>
+                <div>
+
+
+                    <button class="flex mx-auto bg-red-400  text-white font-bold py-2.5 px-14 border border-black text-center 
+                  -700 rounded">GET IN TOUCH
+
+                    </button>
+                    
+
+                </div>
+                
+
             </div>
+            <p>
+            <div class="flex justify-center items-center p-10">
+                <div class="pr-2"><img class="h-8 w-8 object-cover object-center mx-auto border-0" src="/image/facebook.svg" alt="blog"></img> </div>
+                <div class="pr-2"><img class="h-8 w-8 object-cover object-center mx-auto border-0" src="/image/702299.png" alt="blog"></img></div>
+
+
+                <div class="pr-2" ><img class="h-8 w-8 object-cover object-center mx-auto border-0" src="/image/702281.png" alt="blog"></img></div>
+                <div class="pr-2" ><img class="h-8 w-8 object-cover object-center mx-auto border-0 " src="/image/images.png" alt="blog"></img></div>
+                <div class="pr-2" ><img class="h-8 w-8 object-cover object-center mx-auto border-0 " src="/image/images (1).jpg" alt="blog"></img></div>
+                <div class="pr-2" ><img class="h-8 w-8 object-cover object-center mx-auto border-0 " src="/image/download.png" alt="blog"></img></div>
+
+            </div>  
+            
+            </p>
         </div>
+      
+      
+       
+  
+
     )
 }
 export default Tree;
