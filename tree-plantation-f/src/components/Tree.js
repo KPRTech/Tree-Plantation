cb9f30b6410e6350c07c14bb4d4d4371e518b51f
Sumyia <sumyia.mubassira@gmail.com>
2023-02-20 00:00:59 +0600

    image add

--- tree-plantation-f/src/components/Tree.js
+++ tree-plantation-f/src/components/Tree.js
@@ -55,7 +55,12 @@ const Tree = () => {
             </div>
             <p>
             <div class="flex justify-center items-center p-10">
+               
+                
                 <div class="pr-2"><img class="h-8 w-8 object-cover object-center mx-auto border-0" src="/image/facebook.svg" alt="blog"></img> </div>
+              
+                
+                
                 <div class="pr-2"><img class="h-8 w-8 object-cover object-center mx-auto border-0" src="/image/702299.png" alt="blog"></img></div>
 
 
@@ -67,14 +72,70 @@ const Tree = () => {
             </div>  
             
             </p>
+            <section class="text-gray-600 body-font ">
+
+                <div class="container px-5 py-35 mx-auto">
+                    <div class="flex flex-col text-center w-full mb-20">
+                        <h1 class="sm:text-4xl text-2xl font-medium title-font mb-4 text-black font-Weight-900">OUR VISION</h1>
+                        <p class="lg:w-2/3 mx-auto leading-relaxed text-gray ">We want to make it simple for anyone to help the environment by planting trees. Together we can restore</p>
+                        <p class="lg:w-2/3 mx-auto leading-rellaxed text-gray"> forests, create habitat for biodiversity, and make a positive social impact around the world </p>
+                        <p className=" font-bold mb-3 text-black p-4 font-size-20px text-2xl">
+                        We plant one tree with every dollar donated.
+                        </p>
+                    </div>
+                    <div class="flex flex-wrap -m-4 text-center">
+                        <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                        </div>
+                    </div>
+                </div>
+            </section>
+            
+        <div>
+            <div className={css.bg1}>
+
+                <h1 style={{
+                    text:"center",
+                    color: "#ffff",
+                    fontFamily: "Ralway",
+                    fontWeight: "900",
+                    fontSize: "20px",
+                    marginBottom: "0",
+                    lineHeight: "1em",
+                    border: "0",
+                    paddingLeft:"90px"
+                    
+
+                }}>WANT TO HELP MAKE A POSITIVE IMPACT FOR THE ENVIRONMENT?
+                    </h1>
+
+               
+                <div>
+
+
+                    <button>
+                 
+
+                    </button>
+                    
+
+                </div>
+                
+
+            </div>
+            
+            
+        </div>   
         </div>
+          
       
       
        
   
 
     )
+    
 }
+
 export default Tree;
 
 
