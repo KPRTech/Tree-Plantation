da2a2bb1bb43555ac471fbf1d98b32c66680f9b5
Sumyia <sumyia.mubassira@gmail.com>
2023-02-22 19:00:19 +0600

    image add

--- tree-plantation-f/src/components/Tree.js
+++ tree-plantation-f/src/components/Tree.js
@@ -168,15 +168,37 @@ const Tree = () => {
             <div className={css.bg4}>
 
               
-                    <h1 class="text-center sm:text-7xl text-2xl font-medium title-font mb-80 text-white font-Weight-10 font-size-100px">OUR PROCESS</h1>
-                   
-            </div>
+                    <h1 class="text-center sm:text-5xl text-2xl font-medium title-font mb-30 text-white font-Weight-10 font-size-100px">OUR PROCESS</h1>
+                    <p className={css.bg5}>
+            </p>
 
 
- <div className={css.bg5}>
+ 
            
                  </div> 
-           
+
+           <section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap -mx-4 -mb-10 text-center">
+      <div class="sm:w-1/2 mb-10 px-4">
+        <div class="rounded-lg h-64 overflow-hidden">
+          
+        </div>
+        <h2 class="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">Buy YouTube Videos</h2>
+        <p class="leading-relaxed text-base">Williamsburg occupy sustainable snackwave gochujang. Pinterest cornhole brunch, slow-carb neutra irony.</p>
+        <button class="flex mx-auto mt-6 text-white bg-indigo-500 border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
+      </div>
+      <div class="sm:w-1/2 mb-10 px-4">
+        <div class="rounded-lg h-64 overflow-hidden">
+          
+        </div>
+        <h2 class="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">The Catalyzer</h2>
+        <p class="leading-relaxed text-base">Williamsburg occupy sustainable snackwave gochujang. Pinterest cornhole brunch, slow-carb neutra irony.</p>
+        <button class="flex mx-auto mt-6 text-white bg-indigo-500 border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
+      </div>
+    </div>
+  </div>
+</section>
 
         </div>
 
